51b990dd8bce0832bb413d61199e074020db62ec
gomes97 <gomathi9717@gmail.com>
2017-06-22 14:50:15 +0530

    Update delvowel.py

--- delvowel.py
+++ delvowel.py
@@ -1,10 +1,12 @@
 s=raw_input()
 s1=s[::-1]
 li=[]
+h=[]
 for i in range(len(s1)):
 	li.append(s1[i])
 for i in range(len(li)):
 	if li[i]=='a' or li[i]=='e' or li[i]=='i' or li[i]=='o' or li[i]=='u':
 		li[i]=' '
-k=''.join(li)
-print(k)
+
+h=''.join(li).split()
+print(''.join(h))
