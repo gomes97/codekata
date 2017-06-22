de39d5edf1e6f5c921f7459635dabdca27df9993
gomes97 <gomathi9717@gmail.com>
2017-06-22 11:28:27 +0530

    Create mindiff.py

---
+++ mindiff.py
@@ -0,0 +1,11 @@
+h=input("enter hour\n")
+m=input("enter minute\n")
+s=input("enter seconds\n")
+h1=input("enter hour\n")
+m1=input("enter minute\n")
+s1=input("enter second\n")
+print("minutes difference between two times is\n")
+if(m>m1):
+	print(m-m1)
+else:
+	print(m1-m)
